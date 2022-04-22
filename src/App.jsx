7b522777d3048aa81afd7d2cc49310ae0ee0626f
Tomás Aranda <tomasdeveloperfrontend@gmail.com>
2022-04-22 07:57:34 -0300

    add recipes popular

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pages from './pages/Pages';
+
+const App = () => {
+	return (
+		<>
+			<Pages />
+		</>
+	);
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Popular from '../components/Popular';
+import Veggie from '../components/Veggie';
+
+const Home = () => {
+	return (
+		<>
+			<Veggie />
+			<Popular />
+		</>
+	);
+};
+
+export default Home;
