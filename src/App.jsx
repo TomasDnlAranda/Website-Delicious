243caf77d7f8202e3362c1fb549bd1b5aa1f4a17
Tomás Aranda <tomasdeveloperfrontend@gmail.com>
2022-04-27 06:24:36 -0300

    readme

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Pages from './pages/Pages';
 import { themeLight } from './components/Theme';
 import { ThemeProvider } from 'styled-components';
+import Category from './components/Category';
 
 const App = () => {
 	return (
 		<>
 			<ThemeProvider theme={themeLight}>
+				<Category />
 				<Pages />
 			</ThemeProvider>
 		</>
